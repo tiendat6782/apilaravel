668d47a9b29821576f2735b5bc26c32773cc1de0
Hoan <hoanncph21285@fpt.edu.vn>
2023-12-17 09:46:04 +0700

    Add Brand component to Home page

--- front-end/app/(root)/page.jsx
+++ front-end/app/(root)/page.jsx
@@ -1,10 +1,12 @@
 import HomeLayout from "@/components/layout/HomeLayout";
+import Brand from "@/components/section/home/Brand";
 import Cover from "@/components/section/home/Cover";
 
 export default function Home() {
   return (
     <HomeLayout>
       <Cover />
+      <Brand />
     </HomeLayout>
   );
 }
--- front-end/components/section/home/Brand.jsx
+++ front-end/components/section/home/Brand.jsx
@@ -1,7 +1,39 @@
+import "@/public/css/brand.css";
 import React from "react";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="overflow-hidden">
+      <div className="relative flex min-w-full select-none items-center justify-center overflow-visible p-[128px] ">
+        <div className=" z-[1] ml-[12px] flex h-[40px] w-[40px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className=" z-[1] ml-[12px] flex h-[56px] w-[56px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50 backdrop-blur">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className=" z-[1] ml-[12px] flex h-[72px] w-[72px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50 backdrop-blur">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className=" z-[1] ml-[12px] flex h-[96px] w-[96px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50 backdrop-blur">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className=" z-[1] ml-[12px] flex h-[72px] w-[72px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50 backdrop-blur">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className=" z-[1] ml-[12px] flex h-[56px] w-[56px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50 backdrop-blur">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className=" z-[1] ml-[12px] flex h-[40px] w-[40px] shrink-0 items-center justify-center rounded-full border border-slate-100 bg-slate-50 backdrop-blur">
+          <div className="nike h-16 w-16 bg-slate-800"></div>
+        </div>
+        <div className="absolute left-1/2 top-1/2 z-0 ml-[6px] h-[242px] w-[242px] -translate-x-1/2 -translate-y-1/2 overflow-visible">
+          <div className="absolute left-1/2 top-1/2 h-[128px] w-[128px] origin-center  -translate-x-1/2 -translate-y-1/2 rounded-full border border-slate-100 "></div>
+          <div className="absolute left-1/2 top-1/2 h-[192px] w-[192px] origin-center  -translate-x-1/2 -translate-y-1/2 rounded-full border border-slate-100 "></div>
+          <div className="absolute left-1/2 top-1/2 h-[250px] w-[250px] origin-center  -translate-x-1/2 -translate-y-1/2 rounded-full border border-slate-100 "></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
